38f8ebf6a22f8eb9fb2523787337b96b30729e65
Gozantia <lordzantiago@gmail.com>
2020-08-30 21:52:26 -0500

    Updates

---
+++ precache-manifest.90c9ef540d14eb35faebc51db04979de.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a36e4f78922c4ecbf704cf2d18a1a31",
+    "url": "/markdownpreviewer/index.html"
+  },
+  {
+    "revision": "6ee7c3968a2a058de946",
+    "url": "/markdownpreviewer/static/css/2.00839ece.chunk.css"
+  },
+  {
+    "revision": "0b0fcd1ffe343d189a31",
+    "url": "/markdownpreviewer/static/css/main.af8306ae.chunk.css"
+  },
+  {
+    "revision": "6ee7c3968a2a058de946",
+    "url": "/markdownpreviewer/static/js/2.696b3eb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdownpreviewer/static/js/2.696b3eb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b0fcd1ffe343d189a31",
+    "url": "/markdownpreviewer/static/js/main.d2f18f43.chunk.js"
+  },
+  {
+    "revision": "896891f853919999c325",
+    "url": "/markdownpreviewer/static/js/runtime-main.deb89438.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/markdownpreviewer/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/markdownpreviewer/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/markdownpreviewer/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/markdownpreviewer/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/markdownpreviewer/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
